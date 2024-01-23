671a928a07733cfecfaf383bf60912f1ff9b2575
telontelvarost <telontelvarost@gmail.com>
2024-01-22 19:51:00 -0500

    ISSUE-11 - Remove unused imports

--- src/main/java/com/github/telvarost/misctweaks/mixin/TileEntityDispenserMixin.java
+++ src/main/java/com/github/telvarost/misctweaks/mixin/TileEntityDispenserMixin.java
@@ -2,17 +2,14 @@ package com.github.telvarost.misctweaks.mixin;
 
 import com.github.telvarost.misctweaks.Config;
 import com.github.telvarost.misctweaks.ModHelper;
-import net.minecraft.block.BlockBase;
 import net.minecraft.inventory.InventoryBase;
 import net.minecraft.item.ItemInstance;
-import net.minecraft.level.Level;
 import net.minecraft.tileentity.TileEntityBase;
 import net.minecraft.tileentity.TileEntityDispenser;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
