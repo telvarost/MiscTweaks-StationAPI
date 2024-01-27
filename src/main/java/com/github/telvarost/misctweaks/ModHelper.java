a717867c7de9893dbd209cdc03995e10fc400479
telontelvarost <telontelvarost@gmail.com>
2024-01-27 00:43:13 -0500

    UDOC-0 - Update version number and add mod helper fields

--- src/main/java/com/github/telvarost/misctweaks/ModHelper.java
+++ src/main/java/com/github/telvarost/misctweaks/ModHelper.java
@@ -3,6 +3,8 @@ package com.github.telvarost.misctweaks;
 import net.minecraft.block.BlockBase;
 import net.minecraft.item.ItemBase;
 
+import java.util.ArrayList;
+
 public class ModHelper {
     public static void AttemptToSetStackSizeOfFluids()
     {
@@ -42,6 +44,13 @@ public class ModHelper {
     }
 
     public static class ModHelperFields {
+
+        public static Boolean cancelDestroyBlocks = false;
+
+        public static Integer numberOfDamagedBlocks = 0;
+
+        public static Integer numberOfGhastFireballs = 0;
+
         public static Boolean blocksAndItemsRegistered = false;
 
         public static Integer emptySlotAvailable = -1;
