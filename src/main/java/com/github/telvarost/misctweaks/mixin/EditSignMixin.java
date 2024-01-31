0a1a4b85ac42745ec7cdd8fa7ffe52aa7a5327c2
telontelvarost <telontelvarost@gmail.com>
2024-01-30 23:23:33 -0500

    UDOC-0 - Improve compatibility

--- src/main/java/com/github/telvarost/misctweaks/mixin/EditSignMixin.java
+++ src/main/java/com/github/telvarost/misctweaks/mixin/EditSignMixin.java
@@ -10,7 +10,9 @@ import net.minecraft.tileentity.TileEntitySign;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static net.minecraft.util.CharacterUtils.validCharacters;
@@ -23,37 +25,19 @@ public class EditSignMixin extends ScreenBase {
 
     @Shadow private TileEntitySign entity;
 
-    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
-    protected void keyPressed(char c, int i, CallbackInfo ci) {
+    @ModifyConstant(method = "keyPressed", constant = @Constant(intValue = 15))
+    protected int miscTweaks_keyPressed(int value) {
         if (!Config.ConfigFields.enableColorSignsWithDye) {
-            return;
-        }
+            return 15;
+        } else {
+            int lineLimit = 15;
 
-        if (i == 200) {
-            this.cursorPos = this.cursorPos - 1 & 3;
-        }
+            /** - Allow first instance of a section character with its modifier */
+            if (this.entity.lines[this.cursorPos].contains("§")) {
+                lineLimit = lineLimit + 2;
+            }
 
-        if (i == 208 || i == 28) {
-            this.cursorPos = this.cursorPos + 1 & 3;
+            return lineLimit;
         }
-
-        if (i == 14 && this.entity.lines[this.cursorPos].length() > 0) {
-            this.entity.lines[this.cursorPos] = this.entity.lines[this.cursorPos].substring(0, this.entity.lines[this.cursorPos].length() - 1);
-        }
-
-        /** - Skip first instance of a section character with its modifier */
-        int lineLength = this.entity.lines[this.cursorPos].length();
-        if (this.entity.lines[this.cursorPos].contains("§")) {
-            lineLength = lineLength - 2;
-        }
-
-        if (validCharacters.indexOf(c) >= 0 && lineLength < 15) {
-            StringBuilder var10000 = new StringBuilder();
-            String[] var10002 = this.entity.lines;
-            int var10004 = this.cursorPos;
-            var10002[var10004] = var10000.append(var10002[var10004]).append(c).toString();
-        }
-
-        ci.cancel();
     }
 }
--- src/main/java/com/github/telvarost/misctweaks/mixin/TileEntitySignMixin.java
+++ src/main/java/com/github/telvarost/misctweaks/mixin/TileEntitySignMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TileEntitySign.class)
@@ -17,29 +18,25 @@ public class TileEntitySignMixin extends TileEntityBase {
 
     @Shadow public String[] lines;
 
-    @Inject(method = "readIdentifyingData", at = @At("HEAD"), cancellable = true)
-    public void readIdentifyingData(CompoundTag arg, CallbackInfo ci) {
+    @Redirect(
+            method = "readIdentifyingData",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/String;substring(II)Ljava/lang/String;"
+            )
+    )
+    public String miscTweaks_readIdentifyingDataSubstring(String instance, int beginIndex, int endIndex) {
         if (!Config.ConfigFields.enableColorSignsWithDye) {
-            return;
-        }
-
-        this.needsInitializing = false;
-        super.readIdentifyingData(arg);
-
-        for(int var2 = 0; var2 < 4; ++var2) {
-            this.lines[var2] = arg.getString("Text" + (var2 + 1));
+            return instance.substring(beginIndex, endIndex);
+        } else {
+            int lineLimit = 15;
 
             /** - Allow first instance of a section character with its modifier */
-            int lineLimit = 15;
-            if (this.lines[var2].contains("§")) {
+            if (instance.contains("§")) {
                 lineLimit = lineLimit + 2;
             }
 
-            if (this.lines[var2].length() > lineLimit) {
-                this.lines[var2] = this.lines[var2].substring(0, lineLimit);
-            }
+            return instance.substring(beginIndex, lineLimit);
         }
-
-        ci.cancel();
     }
 }
